74d285690a3dde9aaa277f48064b21e775d61f9d
Emin Dedic <emin.dedic@ws-mbp121-135.local>
2019-03-04 22:50:33 +0100

    Tested other headers

--- src/test/java/ResponseHeaders.java
+++ src/test/java/ResponseHeaders.java
@@ -9,6 +9,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.testng.AssertJUnit.assertEquals;
 
@@ -40,5 +42,40 @@ public class ResponseHeaders extends BaseClass {
         ContentType content = ContentType.getOrDefault(response.getEntity());
         assertEquals(content.getMimeType(), "application/json");
     }
+
+
+    @Test
+    public void  serverIsGithub() throws IOException {
+
+        HttpGet get = new HttpGet(BASE_ENDPOINT);
+        response = client.execute(get);
+        
+        String headerValue = getHeader(response, "Server");
+    }
+
+    private String getHeader(CloseableHttpResponse response, String headerName) {
+
+        //Get all headers
+        Header[] headers = response.getAllHeaders();
+        List<Header> httpHeaders = Arrays.asList(headers);
+        String returnHeader = "";
+
+        //Loop over headers list
+
+        for (Header header : headers) {
+            if(headerName.equalsIgnoreCase(header.getName())) {
+                returnHeader = header.getValue();
+            }
+        }
+
+        //If no header found - throw an exception
+        if(returnHeader.isEmpty()) {
+            throw new RuntimeException("Didn't fint the header: " + headerName);
+        }
+
+        //Return header
+        return returnHeader;
+    }
+
 
 }
