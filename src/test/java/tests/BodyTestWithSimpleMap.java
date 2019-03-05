ee68641a1d5aaf86a42eb6012f56dfce566de2ce
emindedic <emindedic@gmail.com>
2019-03-05 15:17:40 +0100

    Test users ID

--- src/test/java/tests/BodyTestWithSimpleMap.java
+++ src/test/java/tests/BodyTestWithSimpleMap.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 
+import static base.User.ID;
 import static base.User.LOGIN;
 
 
@@ -28,6 +29,23 @@ public class BodyTestWithSimpleMap extends BaseClass {
 
         String loginValue = (String) getValueFor(jsonObject, LOGIN);
         Assert.assertEquals(loginValue, "emindedic");
+    }
+
+    
+    @Test
+    public void returnCorrectId() throws IOException {
+
+        HttpGet getBody = new HttpGet(BASE_ENDPOINT + "/users/emindedic");
+        response = client.execute(getBody);
+
+        String jsonBody = EntityUtils.toString(response.getEntity());
+        System.out.println(response);
+
+
+        JSONObject jsonObject = new JSONObject(jsonBody);
+
+        Integer loginValue = (Integer) getValueFor(jsonObject, ID);
+        Assert.assertEquals(loginValue, Integer.valueOf(113345678));
     }
 
     private Object getValueFor(JSONObject jsonObject, String login) {
